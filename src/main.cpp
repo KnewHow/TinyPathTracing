565407cbf4582ded3d833d74927394e37f7ce877
Knewhow <how.yuangh@gmail.com>
2021-04-13 08:35:36 +0800

    init workspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+#include "Vector.hpp"
 
 int main() {
     std::cout << "Hello PT" << std::endl;
+    tinyMath::vec3f v = tinyMath::vec3f(2.0f);
+    std::cout << v << std::endl;
+    return 0;
 }
